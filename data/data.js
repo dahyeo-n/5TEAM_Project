032fb1f85d17820c4100701badf771f5ff2a1857
unknown <qpscm662@naver.com>
2024-01-10 20:57:45 +0900

    fix: 입력기능

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 // 토글기능 구현
 const toggle = document.querySelector('#toggle');
 const toggleVal = document.querySelector('.toggleVal');
-let toggleRepeat = true;
+let toggleRepeat = true; //디폴트 값 설정.
 toggle.addEventListener('click', function () {
   if (toggleRepeat === true) {
     toggleVal.style.display = 'block';
@@ -13,9 +13,9 @@ toggle.addEventListener('click', function () {
 });
 
 //입력기능 구현
-const inputBtn = document.querySelector('.input-btn');
-const valueBox = document.querySelector('.valuetest');
-let inputArray = [];
+const inputBtn = document.querySelector('#inputBtn');
+const createForm = document.querySelector('.createForm');
+let inputArray = []; // 클릭시 각 value들을 저장하기 위해 빈 배열 생성
 inputBtn.addEventListener('click', function (e) {
   e.preventDefault();
   let nameinput = document.querySelector('#floatingInput').value;
@@ -30,7 +30,11 @@ inputBtn.addEventListener('click', function (e) {
     starCnt: star,
   });
   //html 추가
-  let temp_hteml = `<div class="valueBox"><p>이름:${nameinput} 리뷰:${review}  별점:${star}</p></div>`;
-  valueBox.insertAdjacentHTML('beforeend', temp_hteml);
-  console.log(inputArray);
+  let temp_hteml = `<div class="valueBox">
+    <p>이름:${nameinput} 리뷰:${review}  별점:${star} 
+      <button type="button" class="btn btn-dark">수정</button>
+      <button type="button" class="btn btn-danger">삭제</button>
+    </p>
+  </div>`;
+  createForm.insertAdjacentHTML('beforeend', temp_hteml);
 });
