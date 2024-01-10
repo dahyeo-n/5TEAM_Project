25017ff49e5805569dca2c26879d393b454982c9
unknown <qpscm662@naver.com>
2024-01-10 19:49:52 +0900

    feat: 입력기능

--- data/data.js
+++ data/data.js
@@ -13,3 +13,24 @@ toggle.addEventListener('click', function () {
 });
 
 //입력기능 구현
+const inputBtn = document.querySelector('.input-btn');
+const valueBox = document.querySelector('.valuetest');
+let inputArray = [];
+inputBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  let nameinput = document.querySelector('#floatingInput').value;
+  let pwinput = document.querySelector('#floatingPassword').value;
+  let review = document.querySelector('.review').value;
+  let star = document.querySelector('.starCnt').value;
+  //클릭 될때마다 inputArray에 push. 각value는 inputArray에 있는상태.
+  inputArray.push({
+    name: nameinput,
+    pw: pwinput,
+    reviewInput: review,
+    starCnt: star,
+  });
+  //html 추가
+  let temp_hteml = `<div class="valueBox"><p>이름:${nameinput} 리뷰:${review}  별점:${star}</p></div>`;
+  valueBox.insertAdjacentHTML('beforeend', temp_hteml);
+  console.log(inputArray);
+});
