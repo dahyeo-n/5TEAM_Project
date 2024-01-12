b63a6a269585b2aa08e76812bf1c5180b3822748
unknown <qpscm662@naver.com>
2024-01-12 20:15:26 +0900

    feat: 삭제기능 추가

--- data/data.js
+++ data/data.js
@@ -14,8 +14,7 @@ toggle.addEventListener('click', function () {
 
 //입력기능 구현
 const inputBtn = document.querySelector('#inputBtn');
-const createForm = document.querySelector('.createForm');
-
+const createForm = document.querySelector('.createForm'); //reviewList로변경
 let inputArrayGet = window.localStorage.getItem('data'); // 로컬 데이터 가져오기
 let inputArrayVal = JSON.parse(inputArrayGet); // 가져온 로컬 데이터 배열로 다시 전환
 let inputArray = inputArrayVal || []; // 가져온 로컬에 push하기 , 비어있어도 push가능하게 빈 배열 추가
@@ -27,6 +26,7 @@ inputBtn.addEventListener('click', function (e) {
   let review = document.querySelector('.review').value;
   let star = document.querySelector('.starCnt').value;
   let idVal = new Date().getTime(); //현재 시간으로 id 부여
+
   //클릭시 array에 데이터 푸쉬
   inputArray.push({
     id: idVal,
@@ -38,45 +38,58 @@ inputBtn.addEventListener('click', function (e) {
   let inputArrayString = JSON.stringify(inputArray); //push한 데이터 문자열로 전환
   window.localStorage.setItem('data', inputArrayString); // 전환된 데이터 로컬에 저장
 
-  //클릭시 html 추가
-  let temp_html = `<div class="valueBox" >
+  //html 추가
+  let temp_html = `<div data-id=${idVal} class="valueBox" >
     <p>이름:${nameinput} 리뷰:${review}  별점:${star} 
       <button type="button" class="btn btn-dark">수정</button>
-      <button type="button" class="btn btn-danger">삭제</button>
+      <button type="button" data-id=${idVal} class="btn btn-danger">삭제</button>
     </p>
   </div>`;
   createForm.insertAdjacentHTML('beforeend', temp_html);
+  //삭제 이벤트
+  const delBtns = document.querySelectorAll('.btn-danger');
+  delBtns.forEach((deleteVal, index) => {
+    //deleteBtn이 더 낫다
+    deleteVal.addEventListener('click', function (e) {
+      e.preventDefault();
+      let stringIds = deleteVal.dataset.id;
+      let numIds = parseInt(stringIds);
+      inputArray = inputArray.filter((item) => item.id !== numIds);
+
+      updateLocalStorage();
+      e.target.parentElement.parentElement.remove();
+    });
+  });
 });
+//기존 데이터 읽기
+for (let i = 0; i < inputArray.length; i++) {
+  let item = inputArray[i];
 
-// 삭제 기능 추가
-for (let i = 0; i < inputArrayVal.length; i++) {
-  let item = inputArrayVal[i];
-
-  let temp = `<div class="valueBox" >
+  let temp = `<div data-id=${item.id} class="valueBox" >
     <p>이름:${item.name} 리뷰:${item.reviewInput}  별점:${item.starCnt} 
       <button type="button" class="btn btn-dark">수정</button>
-      <button type="button" data-id="${item.id}" class="btn btn-danger">삭제</button>
+      <button type="button" data-id=${item.id} class="btn btn-danger">삭제</button>
     </p>
   </div>`;
   createForm.insertAdjacentHTML('beforeend', temp);
 }
-
-const delBtn = document.querySelectorAll('.btn-danger');
-delBtn.forEach((deleteVal, index) => {
+// 삭제기능 추가 , ⭐⭐⭐⭐⭐ 입력버튼 누르고 그린 html에도 삭제버튼 클릭 이벤트를 걸어줘야함
+const delBtns = document.querySelectorAll('.btn-danger');
+delBtns.forEach((deleteVal, index) => {
+  //deleteBtn이 더 낫다
   deleteVal.addEventListener('click', function (e) {
     e.preventDefault();
     let stringIds = deleteVal.dataset.id;
     let numIds = parseInt(stringIds);
-    inputArrayVal = inputArrayVal.filter((item) => item.id !== numIds);
-    console.log(inputArrayVal); //다시 봐야함
+    inputArray = inputArray.filter((item) => item.id !== numIds);
+
+    updateLocalStorage();
+
+    e.target.parentElement.parentElement.remove();
   });
 });
 
-// createForm.insertAdjacentHTML('beforeend', temp);
-// let delBtn = document.querySelectorAll('#delBtn');
-// let del = delBtn[i];
-// del.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   let data = inputArrayVal; //전체 배열안에 객체형태
-//   let delData = data[i].id; //클릭한 id
-// });
+function updateLocalStorage() {
+  let deleteString = JSON.stringify(inputArray);
+  window.localStorage.setItem('data', deleteString);
+}
