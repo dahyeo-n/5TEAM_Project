9e878f87eefb53aa2be50caf6bf37e54a96dd0f3
unknown <qpscm662@naver.com>
2024-01-15 03:45:03 +0900

    feat: 수정기능추가

--- data/data.js
+++ data/data.js
@@ -12,162 +12,186 @@ toggle.addEventListener('click', function () {
   }
 });
 
-//입력기능 구현
-const inputBtn = document.querySelector('#inputBtn');
-const reviseBtn = document.querySelector('#reviseBtn'); //수정확인버튼
-const createForm = document.querySelector('.createForm'); //reviewList로변경
-let inputArrayGet = window.localStorage.getItem('data'); // 로컬 데이터 가져오기
-let inputArrayVal = JSON.parse(inputArrayGet); // 가져온 로컬 데이터 배열로 다시 전환
-let inputArray = inputArrayVal || []; // 가져온 로컬에 push하기 , 비어있어도 push가능하게 빈 배열 추가
-let nameinput = document.querySelector('#floatingInput');
-let pwinput = document.querySelector('#floatingPassword');
-let review = document.querySelector('.review');
-let star = document.querySelector('.starCnt');
-//입력확인 로직
+//전역 변수
+const inputBtn = document.querySelector('#inputBtn'); //입력 버튼
+const reviseBtn = document.querySelector('#reviseBtn'); //수정완료 버튼
+const createForm = document.querySelector('.createForm'); //리뷰 리스트들 부모
+let inputArrayGet = window.localStorage.getItem('data'); // 로컬 데이터 가져오기 (문자열형태)
+let inputArrayVal = JSON.parse(inputArrayGet); // 배열로 전환된 로컬 데이터
+let inputArray = inputArrayVal || []; // 배열로 전환된 로컬 데이터 , 데이터가 없을 경우에 빈배열
+//이름,비밀번호,리뷰,별점 각각의 폼 비어있음
+let nameForm = document.querySelector('#floatingInput');
+let pwForm = document.querySelector('#floatingPassword');
+let reviewForm = document.querySelector('.review');
+let starForm = document.querySelector('.starCnt');
+
+//입력확인버튼
 inputBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  let nameinputval = nameinput.value;
-  let pwinputval = pwinput.value;
-  let reviewval = review.value;
-  let starval = star.value;
+  let nameVal = nameForm.value;
+  let pwVal = pwForm.value;
+  let reviewVal = reviewForm.value;
+  let starVal = starForm.value;
   let idVal = new Date().getTime(); //현재 시간으로 id 부여
 
   //클릭시 array에 데이터 푸쉬
   inputArray.push({
     id: idVal,
-    name: nameinputval,
-    pw: pwinputval,
-    reviewInput: reviewval,
-    starCnt: starval,
+    name: nameVal,
+    pw: pwVal,
+    reviewInput: reviewVal,
+    starCnt: starVal,
   });
   let inputArrayString = JSON.stringify(inputArray); //push한 데이터 문자열로 전환
   window.localStorage.setItem('data', inputArrayString); // 전환된 데이터 로컬에 저장
 
-  //html 추가
+  // html 추가
   let temp_html = `<div data-id=${idVal} class="valueBox" >
-    <p>이름:${nameinputval} 리뷰:${reviewval}  별점:${starval} 
-      <button type="button" class="btn btn-dark">수정</button>
+    <p>이름:${nameVal} 리뷰:${reviewVal}  별점:${starVal} 
+      <button type="button" data-id=${idVal} class="btn btn-dark">수정</button>
       <button type="button" data-id=${idVal} class="btn btn-danger">삭제</button>
       <p class ="pw">비밀번호 입력:<input form="password" type="password" /></p>
+      <p class ="hiddenId" style="display:none" data-id=${idVal}></p>
       <button class="pwBtn">확인</button>
-      
     </p>
   </div>`;
   createForm.insertAdjacentHTML('beforeend', temp_html);
-  //삭제 이벤트
-  const delBtns = document.querySelectorAll('.btn-danger');
-  delBtns.forEach((deleteVal, index) => {
-    //deleteBtn이 더 낫다
-    deleteVal.addEventListener('click', function (e) {
-      e.preventDefault();
-      let stringIds = deleteVal.dataset.id;
-      let numIds = parseInt(stringIds);
-      inputArray = inputArray.filter((item) => item.id !== numIds);
 
-      updateLocalStorage();
-      e.target.parentElement.parentElement.remove();
-    });
-  });
+  // 입력 폼 비우기
+  // nameForm.value = '';
+  // pwForm.value = '';
+  // reviewForm.value = '';
+  // starForm.value = '';
+  window.location.reload();
+
+  // 수정버튼
+  // const reviseBtns = document.querySelectorAll('.btn-dark');
+  // reviseBtns.forEach((reviseBtn) => {
+  //   reviseBtn.addEventListener('click', function (e) {
+  //     e.preventDefault();
+  //     let reviseIdString = reviseBtn.dataset.id;
+  //     let reviseBtnId = parseInt(reviseIdString);
+
+  //     let targetReviewInput = inputArray.find(
+  //       (item) => item.id === reviseBtnId
+  //     );
+  //     let targetName = targetReviewInput.name;
+  //     let targetPw = targetReviewInput.pw;
+  //     let targetReview = targetReviewInput.reviewInput;
+  //     let targetStar = targetReviewInput.starCnt;
+
+  //     nameForm.value = targetName;
+  //     pwForm.value = targetPw;
+  //     reviewForm.value = targetReview;
+  //     starForm.value = targetStar;
+  //   });
+  // });
+  // 삭제버튼
+  // const delBtns = document.querySelectorAll('.btn-danger');
+  // delBtns.forEach((deleteBtn) => {
+  //   deleteBtn.addEventListener('click', function (e) {
+  //     e.preventDefault();
+
+  //     let deleteBtnId = deleteBtn.dataset.id;
+  //     let numIds = parseInt(deleteBtnId);
+  //     inputArray = inputArray.filter((item) => item.id !== numIds);
+
+  //     e.target.parentElement.parentElement.remove();
+
+  //     updateLocalStorage();
+  //   });
+  // });
 });
-//수정완료 로직
-// reviseBtn.addEventListener('click', function (e) {
-//   e.preventDefault();
 
-//   reviseBtns.forEach((reviseBtn) => {
-//     let reviseId = reviseBtn.dataset.id; //클릭한 ID값
-//     let targetReview = inputArray.find((item) => item.id == reviseId); //클릭한 데이터
-//     // 가져온 데이터 저장
-//     let nameVal = targetReview.name; //수정을 누른 이름
-//     let pw = targetReview.pw; //수정을 누른 비번
-//     let reviewInput = targetReview.reviewInput; //수정을 누른 리뷰
-//     let reviseStar = targetReview.starCnt; //수정을 누른 별
+//기존데이터 읽기 및 표시
+function drawing() {
+  for (let i = 0; i < inputArray.length; i++) {
+    let item = inputArray[i];
 
-//     nameinput.value = nameVal;
-//     pwinput.value = pw;
-//     review.value = reviewInput;
-//     star.value = reviseStar;
-//     updateLocalStorage();
-//   });
-// });
-
-function reviseBtnClick() {
-  let updateVal = nameinput.value;
-  let updatePw = pwinput.value;
-  let updatereview = review.value;
-  let updatestar = star.value;
-  let updateinputArrayVal = inputArrayGet;
-  let updateInputArray = inputArray; //set으로 가져옴
-  const test = inputArray.map((obj) => {
-    const { id, ...rest } = obj;
-    return rest;
-  });
-  console.log(test);
-  test.property = updateVal;
-  const targetObj = test[0];
-  const keys = Object.keys(targetObj);
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    const keyInput = targetObj[key];
-    console.log(keyInput);
+    let temp = `<div ${item.id} class="valueBox" >
+      <p>이름:${item.name} 리뷰:${item.reviewInput}  별점:${item.starCnt}
+        <button type="button" data-id=${item.id} class="btn btn-dark">수정</button>
+        <button type="button" data-id=${item.id} class="btn btn-danger">삭제</button>
+        <p class="pw">비밀번호 입력:<input form="password" type="password" /></p>
+        <button class="pwBtn">확인</button>
+      </p>
+    </div>`;
+    createForm.insertAdjacentHTML('beforeend', temp);
   }
 }
-reviseBtnClick();
+drawing();
 
-//기존 데이터 읽기
-for (let i = 0; i < inputArray.length; i++) {
-  let item = inputArray[i];
-
-  let temp = `<div ${item.id} class="valueBox" >
-    <p>이름:${item.name} 리뷰:${item.reviewInput}  별점:${item.starCnt}
-      <button type="button" data-id=${item.id} class="btn btn-dark">수정</button>
-      <button type="button" data-id=${item.id} class="btn btn-danger">삭제</button>
-      <p class="pw">비밀번호 입력:<input form="password" type="password" /></p>
-      <button class="pwBtn">확인</button>
-    </p>
-  </div>`;
-  createForm.insertAdjacentHTML('beforeend', temp);
-}
-
-// 삭제기능 추가 , ⭐⭐⭐⭐⭐ 입력버튼 누르고 그린 html에도 삭제버튼 클릭 이벤트를 걸어줘야함
-
-const delBtns = document.querySelectorAll('.btn-danger');
-delBtns.forEach((deleteVal) => {
-  //deleteBtn이 더 낫다
-  deleteVal.addEventListener('click', function (e) {
+//삭제버튼  입력시 삭제버튼에도 이벤트 걸어 줘야 함 (나중에 함수화 해서 리팩토링)
+let delBtns = document.querySelectorAll('.btn-danger');
+delBtns.forEach((deleteBtn) => {
+  deleteBtn.addEventListener('click', function (e) {
     e.preventDefault();
 
-    let stringIds = deleteVal.dataset.id;
-    let numIds = parseInt(stringIds);
-    inputArray = inputArray.filter((item) => item.id !== numIds);
-
-    updateLocalStorage();
+    let deleteBtnId = deleteBtn.dataset.id; //삭제 버튼의 ID값
+    let numIds = parseInt(deleteBtnId); //정수로 변환
+    inputArray = inputArray.filter((item) => item.id !== numIds); //기존ID랑 다른것들 item에 재할당
 
     e.target.parentElement.parentElement.remove();
+
+    updateLocalStorage();
   });
 });
 
-//수정기능 추가
-
-const reviseBtns = document.querySelectorAll('.btn-dark');
-
+//수정버튼
+let reviseBtns = document.querySelectorAll('.btn-dark');
+let hiddenId = document.querySelectorAll('.hiddenId');
 reviseBtns.forEach((reviseBtn) => {
   reviseBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    let reviseId = reviseBtn.dataset.id; //클릭한 ID값
-    let targetReview = inputArray.find((item) => item.id == reviseId); //클릭한 데이터
-    // 가져온 데이터 저장
-    let nameVal = targetReview.name; //수정을 누른 이름
-    let pw = targetReview.pw; //수정을 누른 비번
-    let reviewInput = targetReview.reviewInput; //수정을 누른 리뷰
-    let reviseStar = targetReview.starCnt; //수정을 누른 별
+    let reviseIdString = reviseBtn.dataset.id;
+    let reviseBtnId = parseInt(reviseIdString); //클릭한 ID값
 
-    nameinput.value = nameVal;
-    pwinput.value = pw;
-    review.value = reviewInput;
-    star.value = reviseStar;
+    let targetReviewInput = inputArray.find((item) => item.id === reviseBtnId); //클릭한 버튼의 정보들
+    // console.log(targetReviewInput);
+
+    // 클릭한 데이터들 저장 (객체분해 할당으로 리팩토링)
+    let targetName = targetReviewInput.name;
+    let targetPw = targetReviewInput.pw;
+    let targetReview = targetReviewInput.reviewInput;
+    let targetStar = targetReviewInput.starCnt;
+    let targetId = reviseBtnId;
+    // console.log(targetName, targetPw, targetReview, targetStar);
+
+    //비어있는 폼에다가 클릭한 데이터들 표시
+    nameForm.value = targetName;
+    pwForm.value = targetPw;
+    reviewForm.value = targetReview;
+    starForm.value = targetStar;
+    hiddenId.value = targetId;
   });
 });
 
+//수정완료버튼
+reviseBtn.addEventListener('click', function () {
+  let reviseName = nameForm.value;
+  let revisePw = pwForm.value;
+  let reviseReview = reviewForm.value;
+  let reviseStar = starForm.value;
+  let idVal = hiddenId.value;
+  // console.log(reviseName, revisePw, reviseReview, reviseStar, idVal);
+
+  let reviseTarget = inputArray.find((item) => item.id === idVal); //수정할 객체
+  //수정된 데이터들
+  reviseTarget.name = reviseName;
+  reviseTarget.pw = revisePw;
+  reviseTarget.reviewInput = reviseReview;
+  reviseTarget.starCnt = reviseStar;
+
+  for (let i = 0; i < inputArray.length; i++) {
+    let inputArrayId = inputArray[i].id; //기존데이터들 ID
+    if (inputArrayId === idVal) {
+      updateLocalStorage();
+      window.location.reload();
+    }
+  }
+});
+
+//로컬업데이트
 function updateLocalStorage() {
   let deleteString = JSON.stringify(inputArray);
   window.localStorage.setItem('data', deleteString);
