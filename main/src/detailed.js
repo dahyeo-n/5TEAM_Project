879e9a44a4c31003c59da7dddde9317c2375e568
DecidedCard <heajun35@gmail.com>
2024-01-15 18:01:59 +0900

    fix: 디테일 페이지 검색기능

--- main/src/detailed.js
+++ main/src/detailed.js
@@ -1,8 +1,20 @@
 import { drawMovieDetails } from './movieDraw.js';
 import { movieVideo } from './movieDraw.js';
+import {detailedPageSearch} from "./search.js";
+
+
 const check = localStorage.getItem('cast');
 drawMovieDetails(check);
 movieVideo(check);
+
+const search = document.querySelector("#search");
+search.addEventListener("submit", (event) => {
+    event.preventDefault();
+    detailedPageSearch();
+});
+
+const searchBtn = document.querySelector("#searchBtn");
+searchBtn.addEventListener("click", detailedPageSearch);
 
 // Return to HOME
 const homeBtn = document.querySelector('#homeBtn');
