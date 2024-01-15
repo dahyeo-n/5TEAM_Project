ba36ec0bae710046d773e8f2df93ad6b89711179
unknown <qpscm662@naver.com>
2024-01-15 20:17:33 +0900

    CSS 통일

--- main/src/data.js
+++ main/src/data.js
@@ -1,7 +1,7 @@
 //전역 변수
 const check = localStorage.getItem('cast'); //클릭한 페이지 ID값 변수명변경
 const inputBtn = document.querySelector('#inputBtn'); //입력 버튼
-const reviseBtn = document.querySelector('#reviseBtn'); //수정완료 버튼
+const hiddenReviseBtn = document.querySelector('#reviseBtn'); //수정완료 버튼
 const createForm = document.querySelector('.createForm'); //리뷰 리스트들 부모
 let inputArrayGet = window.localStorage.getItem('data'); // 로컬 데이터 가져오기 (문자열형태)
 let inputArrayVal = JSON.parse(inputArrayGet); // 배열로 전환된 로컬 데이터
@@ -38,9 +38,8 @@ inputBtn.addEventListener('click', function (e) {
     <p>이름:${nameVal} 리뷰:${reviewVal}  별점:${starVal} 
       <button type="button" data-id=${idVal} class="btn btn-dark">수정</button>
       <button type="button" data-id=${idVal} class="btn btn-danger">삭제</button>
-      <p class ="pw">비밀번호 입력:<input form="password" type="password" /></p>
-      <p class ="hiddenId" style="display:none" data-id=${idVal}></p>
-      <button class="pwBtn">확인</button>
+      <p class ="pw">비밀번호 입력:<input class="blockPwForm" form="password" type="password" />
+      <button class="pwBtn">확인</button></p>
     </p>
   </div>`;
   createForm.insertAdjacentHTML('beforeend', temp_html);
@@ -103,8 +102,8 @@ function drawing() {
       <p>이름:${item.name} 리뷰:${item.reviewInput}  별점:${item.starCnt}
         <button type="button" data-id=${item.id} class="btn btn-dark">수정</button>
         <button type="button" data-id=${item.id} class="btn btn-danger">삭제</button>
-        <p class="pw">비밀번호 입력:<input form="password" type="password" /></p>
-        <button class="pwBtn">확인</button>
+        <p class="pw" style="display:none">비밀번호 입력:<input class="pwInput" form="password" type="password" />
+        <button class="pwBtn">확인</button></p>
       </p>
     </div>`;
     createForm.insertAdjacentHTML('beforeend', temp);
@@ -114,17 +113,34 @@ drawing();
 
 //삭제버튼  입력시 삭제버튼에도 이벤트 걸어 줘야 함 (나중에 함수화 해서 리팩토링)
 let delBtns = document.querySelectorAll('.btn-danger');
+let pwHiddenFrom = document.querySelector('.pw');
+let pwSubmitBtn = document.querySelector('.pwBtn');
+
 delBtns.forEach((deleteBtn) => {
+  // 1.삭제버튼 클릭 , 벨리데이션
   deleteBtn.addEventListener('click', function (e) {
     e.preventDefault();
+    let deleteBtnId = parseInt(deleteBtn.dataset.id); //삭제 버튼의 ID값
 
-    let deleteBtnId = deleteBtn.dataset.id; //삭제 버튼의 ID값
-    let numIds = parseInt(deleteBtnId); //정수로 변환
-    inputArray = inputArray.filter((item) => item.id !== numIds); //기존ID랑 다른것들 item에 재할당
+    pwHiddenFrom.style.display = 'block';
+    pwSubmitBtn.addEventListener('click', function () {
+      //3.비밀번호 인풋 확인버튼 클릭
+      let pwInput = document.querySelector('.pwInput');
+      let pwInputVal = pwInput.value; //입력한 비밀번호
+      let pwTargetId = inputArray.find((idVal) => idVal.id === deleteBtnId); //inputArray의 id값에서 찾기
+      if (pwTargetId.pw === pwInputVal) {
+        inputArray = inputArray.filter((item) => item.id !== numIds); //기존ID랑 다른것들 item에 재할당
+        alert('삭제완료');
 
-    e.target.parentElement.parentElement.remove();
+        e.target.parentElement.parentElement.remove();
 
-    updateLocalStorage();
+        updateLocalStorage();
+        window.location.reload();
+      } else {
+        alert('비밀번호를 똑바로 확인 하세요');
+        window.location.reload();
+      }
+    });
   });
 });
 
@@ -134,6 +150,14 @@ let hiddenId = document.querySelectorAll('.hiddenId');
 reviseBtns.forEach((reviseBtn) => {
   reviseBtn.addEventListener('click', function (e) {
     e.preventDefault();
+    pwHiddenFrom.style.display = 'block';
+    pwSubmitBtn.addEventListener('click', function () {
+      let pwInput = document.querySelector('.pwInput');
+      let pwInputVal = pwInput.value; //입력한 비밀번호
+      console.log(pwInputVal);
+    });
+    inputBtn.style.display = 'none';
+    hiddenReviseBtn.style.display = 'block';
     let reviseIdString = reviseBtn.dataset.id;
     let reviseBtnId = parseInt(reviseIdString); //클릭한 ID값
 
