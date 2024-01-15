87bfd7f69683773cbc6fda1d8ed841b1df0eec03
DecidedCard <heajun35@gmail.com>
2024-01-15 17:21:58 +0900

    feat: 디테일페이지 검색기능 추가

--- main/src/movieDraw.js
+++ main/src/movieDraw.js
@@ -42,7 +42,7 @@ export const drawmovie = async () => {
 };
 
 //로컬스토리지 저장 함수
-function storegeAdd(keyword) {
+export function storegeAdd(keyword) {
     const data = localStorage.setItem("cast", keyword);
     return data;
 }
--- main/src/search.js
+++ main/src/search.js
@@ -1,3 +1,6 @@
+import {fetchmovieList} from "./api.js";
+import {storegeAdd} from "./movieDraw.js";
+
 export const searchMovie = () => {
   const inputform = document.querySelector('#input-form').value.replace(/\s/g, '');
   const checkTitle = document.querySelectorAll(".card-title");
@@ -17,3 +20,23 @@ export const searchMovie = () => {
     }
   }
 };
+
+export const detailedPageSearch = async () => {
+  const movieList = await fetchmovieList();
+  const inputform = document.querySelector('#input-form').value.replace(/\s/g, '');
+  const checkTitle = movieList.map(e => e.title);
+  
+  for (let i = 0; i < checkTitle.length; i++) {
+    const a = checkTitle[i];
+
+    if (inputform === '') {
+      alert("검색어를 입력해주세요");
+      break;
+    } else {
+      if (a.includes(inputform)) {
+        storegeAdd(movieList[i].id);
+      }
+    }
+  }
+  window.location.reload();
+}
